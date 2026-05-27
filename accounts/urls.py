17f2d1e35fb6408a2373c715504d0d1616911438
FlpsMLK <fesdpaula@gmail.com>
2026-05-27 20:43:18 -0300

    fix logout url

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ from .forms import LoginForm
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html', authentication_form=LoginForm), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', views.logout_view, name='logout'),
     path('cadastro/', views.cadastro, name='cadastro'),
     path('perfil/', views.perfil, name='perfil'),
     path('perfil/<int:pk>/', views.perfil_usuario, name='perfil_usuario'),
